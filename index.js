9752cb5413b933664e4e220c73c9500fad04a420
Shakhawat77 <shoadshakhawat7@gmail.com>
2025-09-08 21:55:31 +0600

    adding spanning to the project

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const loadLessons = () => {
-    fetch("https://openapi.programming-hero.com/api/categories")
-        .then((res) => res.json())
-        .then((json) => displayLesson(json.categories));
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.categories));
 };
+const manageSpinner = (status) => {
+  if (status == true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("trees-container").classList.add("hidden");
+  } else {
+    document.getElementById("trees-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+  }
+}
 
 const removeActive = () => {
-    const clickTreeBtn = document.querySelectorAll(".click-tree");
-    clickTreeBtn.forEach(btn => btn.classList.remove("active"));
+  const clickTreeBtn = document.querySelectorAll(".click-tree");
+  clickTreeBtn.forEach(btn => btn.classList.remove("active"));
 }
 
 const loadTrees = (id) => {
-    const url = `https://openapi.programming-hero.com/api/category/${id}`
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            removeActive();
-            const clickBtn = document.getElementById(`btn-${id}`)
-            clickBtn.classList.add("active");
-            displayTrees(data.plants)
-        });
+  manageSpinner(true);
+  const url = `https://openapi.programming-hero.com/api/category/${id}`
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActive();
+      const clickBtn = document.getElementById(`btn-${id}`)
+      clickBtn.classList.add("active");
+      displayTrees(data.plants)
+    });
 };
 // for modal
 const loadDetail = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/plant/${id}`
-    const res = await fetch(url);
-    const details = await res.json();
-    treeDetails(details.plants);
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`
+  const res = await fetch(url);
+  const details = await res.json();
+  treeDetails(details.plants);
 };
 const treeDetails = (tree) => {
-    console.log(tree);
-    const detailsModal = document.getElementById("details-container");
-    detailsModal.innerHTML = `
+  console.log(tree);
+  const detailsModal = document.getElementById("details-container");
+  detailsModal.innerHTML = `
         <div>
     <h2 class="text-xl font-semibold mb-3">${tree.name} </h2>
     <img src=${tree.image} 
@@ -54,18 +64,18 @@ const treeDetails = (tree) => {
   </div>
     `;
 
-    document.getElementById("tree_modal").showModal();
+  document.getElementById("tree_modal").showModal();
 }
 
 const displayTrees = (trees) => {
-    const treesContainer = document.getElementById("trees-container");
-    treesContainer.innerHTML = "";
+  const treesContainer = document.getElementById("trees-container");
+  treesContainer.innerHTML = "";
 
-    trees.forEach(tree => {
+  trees.forEach(tree => {
 
-        const card = document.createElement("div");
+    const card = document.createElement("div");
 
-        card.innerHTML = `      <div class="bg-white p-4 rounded-xl shadow">
+    card.innerHTML = `      <div class="bg-white p-4 rounded-xl shadow">
           <div > <img class="w-full h-40 bg-gray-200 rounded-lg mb-4" src=${tree.image} alt=""></div>
           <h3 onclick="loadDetail(${tree.id})" id="tree-name" class="font-semibold mb-3">${tree.name}          
           </h3>
@@ -78,21 +88,21 @@ const displayTrees = (trees) => {
           <button class="w-full mt-4 bg-green-600 text-white px-4 py-2 rounded-full hover:bg-[#15803D]">Add to Cart</button>
         </div>
         `;
-        treesContainer.append(card);
-    });
-
-}
+    treesContainer.append(card);
+  });
+  manageSpinner(false);
+};
 
 const displayLesson = (lessons) => {
-    const levelContainer = document.getElementById("level-container");
-    levelContainer.innerHTML = "";
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
 
-    for (let lesson of lessons) {
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `<li href="" id="btn-${lesson.id}" onclick="loadTrees( '${lesson.id}')" class=" block hover:bg-[#15803D] hover:text-white text-black px-3 mb-3 py-1 rounded-md click-tree">
+  for (let lesson of lessons) {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `<li href="" id="btn-${lesson.id}" onclick="loadTrees( '${lesson.id}')" class=" block hover:bg-[#15803D] hover:text-white text-black px-3 mb-3 py-1 rounded-md click-tree">
         ${lesson.category_name} </li > `
-        levelContainer.append(btnDiv);
-    }
+    levelContainer.append(btnDiv);
+  }
 };
 
 
